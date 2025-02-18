fee8c05a9101995439ecf3007458b4fdd136a317
kouser-ahamed <kouserahamed.cse.diu@gmail.com>
2025-02-18 13:00:14 +0600

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,99 @@
 import 'package:flutter/material.dart';
 
-class LabClass05 extends StatelessWidget {
-  const LabClass05({super.key});
-/*
-main axis alignment
-cross "     "
+void main() {
+  runApp(App());
+}
 
-overflow handeling
-1:
-*/
+class App extends StatelessWidget {
+  const App({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Basic Flutter UI - 02",
+      title: "Basic UI - 02",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Basic Flutter UI - 02"),
-        ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: SizedBox(
-            // height: double.infinity,
-            height: 600,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: List.generate(
-                  1000,
-                  (indez) => Icon(
-                        Icons.star,
-                        size: indez * 1.0,
-                      )),
-            ),
+          title: Text("Basic UI - 02"),
+          backgroundColor: Colors.blue,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
           ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 244, 244, 244),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Colors.yellow,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Text(
+                          "50% OFF",
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Icon(
+                        Icons.local_hospital_rounded,
+                        size: 40,
+                        color: Colors.blue,
+                      )
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: Image.network(
+                      "https://nikearprod.vtexassets.com/arquivos/ids/1217812-800-800?width=800&height=800&aspect=true",
+                    ),
+                  ),
+                  Text(
+                    "Nike Air Max",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "\$45,000",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                          ),
+                        ),
+                        Text(
+                          "\$50,000",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                              color: Colors.grey,
+                              decoration: TextDecoration.lineThrough),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
-}
-
-void main() {
-  runApp(const LabClass05());
 }
