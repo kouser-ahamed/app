343afdca2cab211d24be11284719666242c837fd
kouser-ahamed <kouserahamed.cse.diu@gmail.com>
2025-02-18 10:02:42 +0600

    init2

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class LabClass05 extends StatelessWidget {
+  const LabClass05({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Basic Flutter UI - 02",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Basic Flutter UI - 02"),
+        ),
+        body: Row(
+          children: [
+            Text(
+              "Hi ,",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 30,
+              ),
+            ),
+            Text(
+              "This is ",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 30,
+              ),
+            ),
+            Text(
+              "Flutter",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 30,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(const LabClass05());
+}
