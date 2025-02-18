978e5faa4df89d2eb08f506c2fdf73a309352fbb
Kouser Ahamed <142486136+kouser-ahamed@users.noreply.github.com>
2025-02-18 19:42:00 +0600

    Final Edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,92 @@
 import 'package:flutter/material.dart';
-
+ 
 void main() {
-  runApp(App());
+  runApp(MyApp());
 }
-
-class App extends StatelessWidget {
-  const App({super.key});
+ 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Basic UI - 02",
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.grey[200],
         appBar: AppBar(
-          title: Text("Basic UI - 02"),
-          backgroundColor: Colors.blue,
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-          ),
+          title: Text('Product Display'),
+          backgroundColor: Colors.black,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              margin: EdgeInsets.all(20),
-              padding: EdgeInsets.all(30),
+        body: Center(
+          child: ProductCard(),
+        ),
+      ),
+    );
+  }
+}
+ 
+class ProductCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(color: Colors.black12, blurRadius: 5, spreadRadius: 2),
+        ],
+      ),
+      width: 234,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
               decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 244, 244, 244),
-                borderRadius: BorderRadius.circular(30),
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(5),
               ),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                            color: Colors.yellow,
-                            borderRadius: BorderRadius.circular(20)),
-                        child: Text(
-                          "50% OFF",
-                          style: TextStyle(
-                            fontSize: 25,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      Icon(
-                        Icons.local_hospital_rounded,
-                        size: 40,
-                        color: Colors.blue,
-                      )
-                    ],
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 30),
-                    child: Image.network(
-                      "https://nikearprod.vtexassets.com/arquivos/ids/1217812-800-800?width=800&height=800&aspect=true",
-                    ),
-                  ),
-                  Text(
-                    "Nike Air Max",
-                    style: TextStyle(fontSize: 30),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 30),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "\$45,000",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 30,
-                          ),
-                        ),
-                        Text(
-                          "\$50,000",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30,
-                              color: Colors.grey,
-                              decoration: TextDecoration.lineThrough),
-                        )
-                      ],
-                    ),
-                  )
-                ],
+              child: Text(
+                '50% OFF',
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
               ),
             ),
-          ],
-        ),
+          ),
+          SizedBox(height: 20),
+          Image.network(
+            'https://nikearprod.vtexassets.com/arquivos/ids/1217812-800-800?width=800&height=800&aspect=true',
+            height: 100,
+          ),
+          SizedBox(height: 11),
+          Text(
+            'Nike Air Shoes',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 6),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '\$500', 
+                style: TextStyle(
+                  decoration: TextDecoration.lineThrough,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(width: 123),
+              Text(
+                '\$450', 
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+            ],
+          ),
+          SizedBox(height: 90),
+          IconButton(
+            icon: Icon(Icons.favorite, color: Colors.blue, size: 28),
+            onPressed: () {},
+          ),
+        ],
       ),
     );
   }
