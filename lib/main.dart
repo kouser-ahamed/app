0297c0034ed75448ebb5d328f352e23aab1b76f3
kouser-ahamed <kouserahamed.cse.diu@gmail.com>
2025-02-18 10:53:02 +0600

    task-2

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 
 class LabClass05 extends StatelessWidget {
   const LabClass05({super.key});
-
+/*
+main axis alignment
+cross "     "
+*/
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,30 +14,39 @@ class LabClass05 extends StatelessWidget {
         appBar: AppBar(
           title: Text("Basic Flutter UI - 02"),
         ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.star,
-              size: 50,
-            ),
-            Icon(
-              Icons.star,
-              size: 50,
-            ),
-            Icon(
-              Icons.star,
-              size: 50,
-            ),
-            Icon(
-              Icons.star,
-              size: 50,
-            ),
-            Icon(
-              Icons.star_outline,
-              size: 50,
-            ),
-          ],
+        body: Container(
+          height: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // crossAxisAlignment: CrossAxisAlignment.cen,
+            children: [
+              Icon(
+                Icons.star,
+                size: 50,
+                color: Colors.yellow,
+              ),
+              Icon(
+                Icons.star,
+                size: 50,
+                color: Colors.yellow,
+              ),
+              Icon(
+                Icons.star,
+                size: 50,
+                color: Colors.yellow,
+              ),
+              Icon(
+                Icons.star,
+                size: 50,
+                color: Colors.yellow,
+              ),
+              Icon(
+                Icons.star_outline,
+                size: 50,
+                color: Colors.yellow,
+              ),
+            ],
+          ),
         ),
       ),
     );
